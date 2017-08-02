5bb870b7cb263370ad4d46622d04bd57cf1bf3f3
Liam-Loveridge <liamloveridge@outlook.com>
2017-08-02 09:12:58 +0100

    Changes to files
    Added new mock wall design as well as a little game that turns the sphere a different colour when you touch an object. Next: implement the audio solution.

--- NP2017-Test-PSMove/Assets/Scripts/PSMoveNetworkControl.cs
+++ NP2017-Test-PSMove/Assets/Scripts/PSMoveNetworkControl.cs
@@ -10,12 +10,16 @@ using MiniJSON;
 /// This script will move an object in the Unity scene
 /// based on the data coming from the Network location
 /// </summary>
+/// 
+
 public class PSMoveNetworkControl : MonoBehaviour {
 
 	private Vector3 currentPosition;
+    private Renderer rend; 
 
-	void OnEnable() {
+    void OnEnable() {
 		PSMoveJSONHandle.OnJSON += GetNetCoords;
+        rend = GetComponent<Renderer>();
 	}
 
 	void OnDisable() {
@@ -24,7 +28,7 @@ public class PSMoveNetworkControl : MonoBehaviour {
 
 	void Update() {
 		transform.position = currentPosition;
-	}
+    }
 
 
 	////////////////////////////////////////
@@ -39,10 +43,59 @@ public class PSMoveNetworkControl : MonoBehaviour {
 
         // Map the position to our game world
         float mappedXPos = data.xPos / 100;
-        float mappedYPos = data.yPos / 100;
+        float mappedYPos = -(data.yPos / 100);
 
+        //Do stuff from here 
         // Update the position of this object
 		currentPosition = new Vector3(mappedXPos, mappedYPos, 0.0f); // The X position is what we're interested in as the camera is positioned to the right of the person in the headset
         Debug.Log("Position: " + currentPosition);
     }
+    void OnTriggerEnter(Collider other)
+    {
+        switch(other.name)
+        {
+            case "Portrait1":
+                rend.material.color = Color.red;
+                break;
+
+            case "Portrait2":
+                rend.material.color = Color.blue;
+                break;
+
+            case "Landscape":
+                rend.material.color = Color.yellow;
+                break;
+
+            case "Fireplace":
+                rend.material.color = Color.green;
+                break;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        switch (other.name)
+        {
+            case "Portrait1":
+                rend.material.color = Color.white;
+                break;
+
+            case "Portrait2":
+                rend.material.color = Color.white;
+                break;
+
+            case "Landscape":
+                rend.material.color = Color.white;
+                break;
+
+            case "Fireplace":
+                rend.material.color = Color.white;
+                break;
+
+        }
+    }
+   
+
+
 }
